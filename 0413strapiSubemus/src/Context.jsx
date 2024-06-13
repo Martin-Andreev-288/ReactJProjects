66f4a30e756686445b33c3eb7618ad4355d10b19
Martin Andreev <martin_i_a@abv.bg>
2024-06-14 00:37:29 +0300

    269 submenu JSX
    zapochvame rabota v/u submenutata (tova, koeto tr da izliza, kogato hovernem v/u Produc/Solutions ili Resources)
    Zasega izglezhda dosta nekadyrno, ponezhe oshte nqmame CSS

--- 0413strapiSubemus/src/Context.jsx
+++ 0413strapiSubemus/src/Context.jsx
@@ -1,10 +1,6 @@
 import { createContext, useState, useContext } from "react";
 
 const AppContext = createContext();
-// zapochvame da dobavqme logika.
-// Kogato hovernem v/u stranicite (t.e. v/u Product/Solutions/Resources)
-// tr da izlizat razni neshta. Zasega izlizat samo id-tata, kato
-// poglednem v components. Za celta izpolzvame onMouseEnter
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [pageId, setPageId] = useState(null);
--- 0413strapiSubemus/src/Submenu.jsx
+++ 0413strapiSubemus/src/Submenu.jsx
@@ -1,4 +1,40 @@
+import { useGlobalContext } from "./Context";
+import sublinks from "./data";
+/* zapochvame rabota v/u submenutata (tova, koeto tr da izliza, kogato
+hovernem v/u Produc/Solutions ili Resources)
+Zasega izglezhda dosta nekadyrno, ponezhe oshte nqmame CSS
+ */
 const Submenu = () => {
-  return <h2>Submenu</h2>;
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  /* console.log(currentPage); rezultatyt izliza kato hovernem s
+  butona v/u zaglavieto na stranicata. Inache pyrvonachalno e
+  undefined
+primer s edna ot stranicite (kakvo logva, kato hovernem):
+{pageId: 'wmy_WIsDwZwDDZxaz-pp_', page: 'product', links: Array(3)}
+  */
+
+  return (
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
