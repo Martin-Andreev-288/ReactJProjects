41b707de554528922efae3413459bd457cef4b37
Martin Andreev <martin_i_a@abv.bg>
2024-06-14 19:31:20 +0300

    272 hideSubmenuPart2
    pravim taka, che submenu-to da izchezva ot koqto i posoka da izlezem
    s mishkata ot nego. Zashtoto predi ne se sluchvashe, kato izlezem s neq
    otdolu/vstrani, a otgore syshto se poluchavashe vremenen stranen rezultat.
    Izpolzvame useRef, getBoundingClientRect() i clientX i clientY, koito
    izvlichame ot event-a

--- 0413strapiSubemus/src/Navbar.jsx
+++ 0413strapiSubemus/src/Navbar.jsx
@@ -1,14 +1,7 @@
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./Context";
 import NavLinks from "./NavLinks";
-/* tuk pravim taka, che kato mahnem kursora ot butonite (product,
-solutions i resources) meniuto, koeto izliza, da se skrie
-Ppc kato minem vyrhu meniuto, to ne trqbva da se skriva, no tova se
-sluchvashe. Zatova v CSS-a dolu na 2 mesta dobavihme align-self: stretch;,
-koeto pravi taka, che goleminata na "zonata" na butonite da se uvelichi
-(t.e. da zaemat tezi height: 5rem, zadadeni za navbar-a, koito tuk na
-tozi screenshot ne se vizhdat). Vizh videoto, ako ne shvashtash
-*/
+
 const Navbar = () => {
   const { openSidebar, setPageId } = useGlobalContext();
   const handleSubmenu = (e) => {
--- 0413strapiSubemus/src/Submenu.jsx
+++ 0413strapiSubemus/src/Submenu.jsx
@@ -1,7 +1,13 @@
+import { useRef } from "react";
 import { useGlobalContext } from "./Context";
 import sublinks from "./data";
+/* pravim taka, che submenu-to da izchezva ot koqto i posoka da izlezem
+s mishkata ot nego. Zashtoto predi ne se sluchvashe, kato izlezem s neq
+otdolu/vstrani, a otgore syshto se poluchavashe vremenen stranen rezultat.
+Izpolzvame useRef, getBoundingClientRect() i clientX i clientY, koito
+izvlichame ot event-a */
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
   const currentPage = sublinks.find((item) => item.pageId === pageId);
   /* console.log(currentPage); rezultatyt izliza kato hovernem s
   butona v/u zaglavieto na stranicata. Inache pyrvonachalno e
@@ -10,8 +16,23 @@ primer s edna ot stranicite (kakvo logva, kato hovernem):
 {pageId: 'wmy_WIsDwZwDDZxaz-pp_', page: 'product', links: Array(3)}
   */
 
+  const submenuContainer = useRef(null);
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
   return (
-    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+    <div
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
