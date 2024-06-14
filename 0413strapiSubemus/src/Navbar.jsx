ffca444dcfffdead4d4f0c245da3cbeaac80ad82
Martin Andreev <martin_i_a@abv.bg>
2024-06-14 18:59:27 +0300

    271 hide submenu part 1
    tuk pravim taka, che kato mahnem kursora ot butonite (product,
    solutions i resources) meniuto, koeto izliza, da se skrie
    Ppc kato minem vyrhu meniuto, to ne trqbva da se skriva, no tova se
    sluchvashe. Zatova v CSS-a dolu na 2 mesta dobavihme align-self: stretch;,
    koeto pravi taka, che goleminata na "zonata" na butonite da se uvelichi
    (t.e. da zaemat tezi height: 5rem, zadadeni za navbar-a, koito tuk na
    tozi screenshot ne se vizhdat). Vizh videoto, ako ne shvashtash

--- 0413strapiSubemus/src/Navbar.jsx
+++ 0413strapiSubemus/src/Navbar.jsx
@@ -1,11 +1,23 @@
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./Context";
 import NavLinks from "./NavLinks";
-
+/* tuk pravim taka, che kato mahnem kursora ot butonite (product,
+solutions i resources) meniuto, koeto izliza, da se skrie
+Ppc kato minem vyrhu meniuto, to ne trqbva da se skriva, no tova se
+sluchvashe. Zatova v CSS-a dolu na 2 mesta dobavihme align-self: stretch;,
+koeto pravi taka, che goleminata na "zonata" na butonite da se uvelichi
+(t.e. da zaemat tezi height: 5rem, zadadeni za navbar-a, koito tuk na
+tozi screenshot ne se vizhdat). Vizh videoto, ako ne shvashtash
+*/
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <h3 className="logo">strapi</h3>
         <button className="toggle-btn" onClick={openSidebar}>
