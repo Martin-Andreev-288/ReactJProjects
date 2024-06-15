071a68978fe1efb7c4f4b8d00a0e6e4001593eec
Martin Andreev <martin_i_a@abv.bg>
2024-06-15 18:12:16 +0300

    273, 274 cart set up
    nachalo na proekta. NE GO ZAPOCHVAME OT 0, zashtoto shte se koncentrirame v/u useReducer

---
+++ 0414cart/src/App.jsx
@@ -0,0 +1,14 @@
+// components
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+// nachalo na proekta. NE GO ZAPOCHVAME OT 0, zashtoto shte se koncentrirame v/u useReducer
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
