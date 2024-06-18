1caef575bde2ee0465c29a6ebbb10f01ad4a8239
Martin Andreev <martin_i_a@abv.bg>
2024-06-18 23:36:49 +0300

    329 searchForm
    search formata, tuk ne pishem e.target.value(), a neshto razlichno. Ideqta e, che tova si syvpada s tova, koeto sme vyveli v search bar-a.

--- 10unsplashImages/src/SearchForm.jsx
+++ 10unsplashImages/src/SearchForm.jsx
@@ -1,4 +1,27 @@
+// search formata, tuk ne pishem e.target.value(), a neshto razlichno. Ideqta e, che tova si syvpada s tova, koeto sme vyveli v search bar-a.
+
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 export default SearchForm;
