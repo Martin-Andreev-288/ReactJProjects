030875c264549eacc5e4e7663af0bfa81dea5983
Martin Andreev <martin_i_a@abv.bg>
2024-06-18 22:38:42 +0300

    325 darkThemeInitialSetup
    vnedrqvane na svetla/tymna tema (izpolzvame react icons). Estestveno, oshte ne raboti, samo ikonite se smenqt na tozi etap

--- 10unsplashImages/src/ThemeToggle.jsx
+++ 10unsplashImages/src/ThemeToggle.jsx
@@ -1,6 +1,18 @@
 import { useGlobalContext } from "./context";
-
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+// vnedrqvane na svetla/tymna tema (izpolzvame react icons). Estestveno, oshte ne raboti, samo ikonite se smenqt na tozi etap
 const ThemeToggle = () => {
-  return <h2>ThemeToggle</h2>;
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon" />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 };
 export default ThemeToggle;
