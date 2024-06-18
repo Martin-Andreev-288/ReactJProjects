89b09117ff1a397dc93907d67c8cd21192dfd9bf
Martin Andreev <martin_i_a@abv.bg>
2024-06-19 00:52:48 +0300

    336 reactQueryDevtools
     kak da instalirame react query dev tools - npm i @tanstack/react-query-devtools, sled tova npm run dev i vizhdame prozorec (tozi dolu vlqvo) where i can find bunch of useful info about my queries.
    Za da go zatvorim - prosto cykame na cveteto ili promenqme initialIsOpen na false

--- 10unsplashImages/src/main.jsx
+++ 10unsplashImages/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </AppProvider>
 );
+
+// kak da instalirame react query dev tools - npm i @tanstack/react-query-devtools, sled tova npm run dev
+// i vizhdame prozorec (tozi dolu vlqvo) where i can find bunch of useful info about my queries.
+// za da go zatvorim - prosto cykame na cveteto ili promenqme initialIsOpen na false
