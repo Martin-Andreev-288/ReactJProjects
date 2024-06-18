a5d8bac022302d5f08ca5f395d5085a322781443
Martin Andreev <martin_i_a@abv.bg>
2024-06-19 00:31:19 +0300

    334 setupQuery
    zapochvame da syzdavame galeriqta, vizhdame kakvo se logva

--- 10unsplashImages/src/Gallery.jsx
+++ 10unsplashImages/src/Gallery.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=pyFtgHOiITJlJrUaat0dNT00TKcEasWWcAzG4n-ULNw&query=office";
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+  console.log(response);
   return <h2>Gallery</h2>;
 };
 export default Gallery;
