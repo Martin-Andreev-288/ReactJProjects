1e3120c5215f4223a87fae6f09785757ad886e27
Martin Andreev <martin_i_a@abv.bg>
2024-06-19 00:45:10 +0300

    335 render images
    i tuk veche image-ite se renderirat!!! Ako smenq tova sled = (primerno ot "office" na "cat", shte pokazva kotenca)

--- 10unsplashImages/src/Gallery.jsx
+++ 10unsplashImages/src/Gallery.jsx
@@ -13,7 +13,44 @@ const Gallery = () => {
       return result.data;
     },
   });
-  console.log(response);
-  return <h2>Gallery</h2>;
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+  if (!results.length) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          ></img>
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
