0b636816b937e7370eba19fb7f10c4fae97fe671
Martin Andreev <martin_i_a@abv.bg>
2024-06-19 22:48:51 +0300

    368 link component
    izpolzvane na Link. Taka ako cyknem na About page - shte ni otvede v neq. Kato vlezem v neq ako cyknem na Home page - shte ni otvede v neq.
    tova raboti na principa na linka, no Link e samo za componenti. Ako iskame da ni prenasochva kym link, koyto ne e component ili fayl ot prilozhenieto - tr pak da go napravim po standartniq nachin, pr:
    <a href='https://johnsmilga.com/'>best web dev courses</a>

--- 12mixmaster/src/pages/HomeLayout.jsx
+++ 12mixmaster/src/pages/HomeLayout.jsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+/* izpolzvane na Link. Taka ako cyknem na About page - shte ni otvede v neq. Kato vlezem v neq ako cyknem na Home page - shte ni otvede v neq.
+tova raboti na principa na linka, no Link e samo za componenti. Ako iskame da ni prenasochva kym link, koyto ne e component ili fayl ot prilozhenieto - tr pak da go napravim po standartniq nachin, pr:
+<a href='https://johnsmilga.com/'>best web dev courses</a> */
 const HomeLayout = () => {
-  return <h1>HomeLayout</h1>;
+  return (
+    <div>
+      <h1>HomeLayout</h1>
+      <Link to="about">About Page</Link>
+    </div>
+  );
 };
 export default HomeLayout;
