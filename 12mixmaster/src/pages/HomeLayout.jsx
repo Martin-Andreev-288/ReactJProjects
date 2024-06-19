17681e8e4be38a1589fb8e834b682bd4225d3d20
Martin Andreev <martin_i_a@abv.bg>
2024-06-20 00:47:20 +0300

    376 page layout
    vmesto za vsqka otdelna stranica da pravim CSS-a, kogato syvpada - mozhem da izpolzvame Outlet komponenta, koyto sybira vsichki komponenti. Taka css-a v index.css za .page se prilaga na
    vsqka stranica

--- 12mixmaster/src/pages/HomeLayout.jsx
+++ 12mixmaster/src/pages/HomeLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+
+/* vmesto za vsqka otdelna stranica da pravim CSS-a, kogato syvpada - mozhem da izpolzvame Outlet komponenta, koyto sybira vsichki komponenti. Taka css-a v index.css za .page se prilaga na
+vsqka stranica (oshte 1 screenshot, za da se vidi) */
 const HomeLayout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <section className="page">
+        <Outlet />
+      </section>
     </>
   );
 };
