40b5a796a84784712ad0388267b7929a0c47361b
Martin Andreev <martin_i_a@abv.bg>
2024-06-20 17:04:42 +0300

    382 fetchDrinks
    fetchvane na napitki, zasega ostavqme searchTerm prazno

--- 12mixmaster/src/pages/Landing.jsx
+++ 12mixmaster/src/pages/Landing.jsx
@@ -1,12 +1,18 @@
 import { useLoaderData } from "react-router-dom";
-// implementirane na loader-a (oshte ne shvashtam kakva e celta na tova tochno)
+import axios from "axios";
+// fetchvane na napitki, zasega ostavqme searchTerm prazno
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
 export const loader = async () => {
-  return "something";
+  const searchTerm = "";
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return <h1>Landing</h1>;
 };
 export default Landing;
