b9c6536a202b804fe4c5a841f9f9a2180bf23393
Martin Andreev <martin_i_a@abv.bg>
2024-06-20 20:11:09 +0300

    387 globalLoading&Context
    ideqta tuk e da se pokazva animaciqta za prezarezhdane, kato sme v systoqnie "loading" (tova e glavnoto neshto)
    Inache malko obyrkvashto, no tova za const value = 'some value'; e za da ni pokazhe how we automatically get this context prop. And essentially by using this prop, we can pass some data from the parent
    where we have the outlet to all of the pages, and not only the pages but to the smallest component, which in our case is the card. Basically, whatever levels deep, you have components you'll still have
    access, which is super cool, so we don't have to set up anything from scratch. Tuk e samo za primer - pokazvame kakvo log-va ({value 'some value'}) i go dekomentirame v CocktaiCard komponenta. Tuk mozhe
    i da ne vzimash predvid tova value = 'some value'

--- 12mixmaster/src/pages/HomeLayout.jsx
+++ 12mixmaster/src/pages/HomeLayout.jsx
@@ -1,14 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-
-/* vmesto za vsqka otdelna stranica da pravim CSS-a, kogato syvpada - mozhem da izpolzvame Outlet komponenta, koyto sybira vsichki komponenti. Taka css-a v index.css za .page se prilaga na
-vsqka stranica (oshte 1 screenshot, za da se vidi) */
+/* ideqta tuk e da se pokazva animaciqta za prezarezhdane, kato sme v systoqnie "loading" (tova e glavnoto neshto)
+Inache malko obyrkvashto, no tova za const value = 'some value'; e za da ni pokazhe how we automatically get this context prop. And essentially by using this prop, we can pass some data from the parent
+where we have the outlet to all of the pages, and not only the pages but to the smallest component, which in our case is the card. Basically, whatever levels deep, you have components you'll still have
+access, which is super cool, so we don't have to set up anything from scratch. Tuk e samo za primer - pokazvame kakvo log-va ({value 'some value'}) i go dekomentirame v CocktaiCard komponenta. Tuk mozhe
+i da ne vzimash predvid tova value = 'some value' */
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isPageLoading = navigation.state === "loading";
+  const value = "some value";
   return (
     <>
       <Navbar />
       <section className="page">
-        <Outlet />
+        {isPageLoading ? (
+          <div className="loading" />
+        ) : (
+          <Outlet context={{ value }} />
+        )}
       </section>
     </>
   );
