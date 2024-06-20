8cbe0e0a69b57c52bdede7750a97f14827a858bf
Martin Andreev <martin_i_a@abv.bg>
2024-06-20 22:32:42 +0300

    393 reactToastifySetup
    dobavqme toastify i pravim taka, che da se zatvarq sled 2 sekundi syobshtenieto

--- 12mixmaster/src/pages/Cocktail.jsx
+++ 12mixmaster/src/pages/Cocktail.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import Wrapper from "../assets/wrappers/CocktailPage";
 const singleCocktailUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
-// snimka na CSS-a na vseki otdelen cocktail
 export const loader = async ({ params }) => {
   const { id } = params;
   const { data } = await axios.get(`${singleCocktailUrl}${id}`);
