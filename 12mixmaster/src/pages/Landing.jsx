debad976498fa375a2def657a72406f3b1d3d00b
Martin Andreev <martin_i_a@abv.bg>
2024-06-20 17:47:23 +0300

    384 cocktailList
    zapochvame da izobrazqvame cocktail list-a (zasega se renderira "CocktaiCard" 25 pyti)

--- 12mixmaster/src/pages/Landing.jsx
+++ 12mixmaster/src/pages/Landing.jsx
@@ -1,18 +1,23 @@
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
-
+import CocktailList from "../components/CocktailList";
+// zapochvame da izobrazqvame cocktail list-a (zasega se renderira "CocktaiCard" 25 pyti)
 const cocktailSearchUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 export const loader = async () => {
   const searchTerm = "";
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  // return { drinks: response.data.drinks, searchTerm };
+  return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
   const { drinks, searchTerm } = useLoaderData();
-  console.log(drinks);
-  return <h1>Landing</h1>;
+
+  return (
+    <>
+      <CocktailList drinks={drinks} />
+    </>
+  );
 };
 export default Landing;
