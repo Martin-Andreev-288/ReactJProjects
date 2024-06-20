e8a56f08d434aa0d3957a1c42889484110726209
Martin Andreev <martin_i_a@abv.bg>
2024-06-20 20:41:40 +0300

    389 singleCocktailRender
    veche imame info za vseki otdelen cocktail, kato go otvorim

--- 12mixmaster/src/pages/Cocktail.jsx
+++ 12mixmaster/src/pages/Cocktail.jsx
@@ -1,8 +1,7 @@
 import { useLoaderData, Link } from "react-router-dom";
 import axios from "axios";
 import Wrapper from "../assets/wrappers/CocktailPage";
-
-// okonchatelno (ideqta e da renderirame koktayla, est oshte ne sme gotovi)
+// veche imame info za vseki otdelen cocktail, kato go otvorim
 const singleCocktailUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
@@ -15,6 +14,52 @@ export const loader = async ({ params }) => {
 
 const Cocktail = () => {
   const { id, data } = useLoaderData();
-  return <h1>Cocktail</h1>;
+
+  const singleDrink = data.drinks[0];
+
+  const {
+    strDrink: name,
+    strDrinkThumb: image,
+    strAlcoholic: info,
+    strCategory: category,
+    strGlass: glass,
+    strInstructions: instructions,
+  } = singleDrink;
+
+  return (
+    <Wrapper>
+      <header>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+        <h3>{name}</h3>
+      </header>
+      <div className="drink">
+        <img src={image} alt={name} className="img" />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions :</span>
+            {instructions}
+          </p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default Cocktail;
