a5a7a4faae12e741ee3443c8b8fec1a03b51cada
Martin Andreev <martin_i_a@abv.bg>
2024-06-21 23:56:10 +0300

    425 useSelector add additional screenshot
    dopylnitelni razqsneniq na predniq screenshot - dostypvame celiq state na prilozhenieto s useSelector.
        Ponezhe v momenta edinstveniq dobaven reducer e cart: cartReducer, mozhem da dostypim samo nego.
        dostypvame amount s destruktirirane i izpolzvame .cart, zashtoto tova ime sme zadali v store.js.
        IZVOD!!! SO THAT'S HOW WE CAN ACCESS DATA FROM OUR SLICE.
        WE NEED TO PICK THE COMPONENT -> import { useSelector } from 'react-redux';
        WE NEED TO USE THE USE SELECTOR (BELOW). AND HERE, WE PASS IN THE FUNCTION -> (store) => store.cart AND AS A PARAMETER OF
        THIS FUNCTION GETS THE ENTIRE STORE. AND THEN WE JUST NEED TO PICK WHAT WE WANNA RETURN. IN THIS CASE, WE WANNA RETYRN CART
        FROM THIS FUNCTION. IT IS AN OBJECT, AND IT REPRESENTS THIS INITIAL STATE (IN cartSlice.js).

--- 14redux-toolkit-tutorial/src/components/Navbar.js
+++ 14redux-toolkit-tutorial/src/components/Navbar.js
@@ -2,8 +2,18 @@ import { CartIcon } from '../icons';
 import { useSelector } from 'react-redux';
 
 const Navbar = () => {
-    // tuk dostypvame celiq state i taka renderirame 0 gore vdqsno
+    /* dopylnitelni razqsneniq na predniq screenshot - dostypvame celiq state na prilozhenieto s useSelector.
+    Ponezhe v momenta edinstveniq dobaven reducer e cart: cartReducer, mozhem da dostypim samo nego.
+    dostypvame amount s destruktirirane i izpolzvame .cart, zashtoto tova ime sme zadali v store.js.
+    IZVOD!!! SO THAT'S HOW WE CAN ACCESS DATA FROM OUR SLICE.
+    WE NEED TO PICK THE COMPONENT -> import { useSelector } from 'react-redux';
+    WE NEED TO USE THE USE SELECTOR (BELOW). AND HERE, WE PASS IN THE FUNCTION -> (store) => store.cart AND AS A PARAMETER OF
+    THIS FUNCTION GETS THE ENTIRE STORE. AND THEN WE JUST NEED TO PICK WHAT WE WANNA RETURN. IN THIS CASE, WE WANNA RETYRN CART
+    FROM THIS FUNCTION. IT IS AN OBJECT, AND IT REPRESENTS THIS INITIAL STATE (IN cartSlice.js).
+    */
     const { amount } = useSelector((store) => store.cart);
+    // pyrvonachalno go bqhme napisali taka:
+    // const amount = useSelector((store) => store.cart.amount);
 
     return (
         <nav>
