2b6b6ad1ab5c40caef255876370c33cc1ed94ca0
Martin Andreev <martin_i_a@abv.bg>
2024-06-21 17:04:38 +0300

    411 additionalAssets
    premestvane na faylove ot starter papkata (vizh vyv fayl lentata tuk vstrani) tuk i info za 2 polezni tailwind extension-a (instalirah pyrviq):
    talwind css intellisence (extension) -> mnogo polezno za tailwind, pokazva ni alternativi/ekvivalenti s klasicheskiq CSS i drugi polezni neshta
    i talwind fold, ako sluchayno mnogoto kod v jsx-a ot tailwind-a ni e mnogo - s tova da se svie, no tova veche e po-malko polezno i instruktora ne go polzva tolkova

--- 13tailwindPortfolio/src/App.jsx
+++ 13tailwindPortfolio/src/App.jsx
@@ -1,5 +1,7 @@
-/* nachalo na proekta - sled vsichki izpylneni operacii ot tailwind sayta (tova sa osnovnite
-nachalni faylove) */
+/* premestvane na faylove ot starter papkata (vizh vyv fayl lentata tuk vstrani) tuk i info za 2 polezni tailwind extension-a (instalirah pyrviq):
+talwind css intellisence (extension) -> mnogo polezno za tailwind, pokazva ni alternativi/ekvivalenti s klasicheskiq CSS i drugi polezni neshta
+i talwind fold, ako sluchayno mnogoto kod v jsx-a ot tailwind-a ni e mnogo - s tova da se svie, no tova veche e po-malko polezno i instruktora ne go polzva tolkova */
+
 const App = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
