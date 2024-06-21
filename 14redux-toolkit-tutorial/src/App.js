7dc67589e8dcc1953b4dba36143feeacdbef365b
Martin Andreev <martin_i_a@abv.bg>
2024-06-21 22:27:21 +0300

    422 store
    suzdavane na store. Napravihme go po malko po-razlichen nachin ot videoto, zashtoto davashe greshla. Vyv videoto instruktoryt izpolzvashe ReactDOM, vmesto createRoot

--- 14redux-toolkit-tutorial/src/App.js
+++ 14redux-toolkit-tutorial/src/App.js
@@ -1,4 +1,3 @@
-// nachalo na proekt s redux. Instalirahme go s npm install @reduxjs/toolkit react-redux
 
 function App() {
   return <h2>Redux Toolkit</h2>;
--- 14redux-toolkit-tutorial/src/index.js
+++ 14redux-toolkit-tutorial/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { store } from './store';
+import { Provider } from 'react-redux';
+// suzdavane na store. Napravihme go po malko po-razlichen nachin ot videoto, zashtoto davashe greshla. Vyv videoto instruktoryt izpolzvashe ReactDOM, vmesto createRoot
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
