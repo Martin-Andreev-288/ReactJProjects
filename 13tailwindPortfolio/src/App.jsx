c448482e5f6bf7f42c1aea767e96ffb619f4d168
Martin Andreev <martin_i_a@abv.bg>
2024-06-21 17:30:55 +0300

    412 navbar
    zapochvame navbar komponent-a

--- 13tailwindPortfolio/src/App.jsx
+++ 13tailwindPortfolio/src/App.jsx
@@ -1,8 +1,10 @@
-/* premestvane na faylove ot starter papkata (vizh vyv fayl lentata tuk vstrani) tuk i info za 2 polezni tailwind extension-a (instalirah pyrviq):
-talwind css intellisence (extension) -> mnogo polezno za tailwind, pokazva ni alternativi/ekvivalenti s klasicheskiq CSS i drugi polezni neshta
-i talwind fold, ako sluchayno mnogoto kod v jsx-a ot tailwind-a ni e mnogo - s tova da se svie, no tova veche e po-malko polezno i instruktora ne go polzva tolkova */
+import Navbar from "./components/NavBar";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 export default App;
