9b4f4cb284d4a1f7590dac645d2858339d6520d1
Martin Andreev <martin_i_a@abv.bg>
2024-06-21 23:16:23 +0300

    425 useSelector
    tuk dostypvame celiq state i taka renderirame 0 gore vdqsno

--- 14redux-toolkit-tutorial/src/store.js
+++ 14redux-toolkit-tutorial/src/store.js
@@ -5,5 +5,3 @@ export const store = configureStore({
         cart: cartReducer,
     },
 });
-/* instalirame redux dev tools v chrome. Kato go napravim, ni se dobavq pole za izbirane redux, ot koeto mozhem da vidim informaciq. Vizh vdqsno. Zasega, est, e malko
-That's why this tool is very useful - because as we'll dispatch our actions, we'll clearly see how our state changes. */
