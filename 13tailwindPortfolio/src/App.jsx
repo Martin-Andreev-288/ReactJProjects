ee46fd9b2dd8056b7d031599d2a70eca61bc0707
Martin Andreev <martin_i_a@abv.bg>
2024-06-21 16:29:36 +0300

    410 project setup
    nachalo na proekta - sled vsichki izpylneni operacii ot tailwind sayta (tova sa osnovnite nachalni faylove)

---
+++ 13tailwindPortfolio/src/App.jsx
@@ -0,0 +1,6 @@
+/* nachalo na proekta - sled vsichki izpylneni operacii ot tailwind sayta (tova sa osnovnite
+nachalni faylove) */
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+export default App;
