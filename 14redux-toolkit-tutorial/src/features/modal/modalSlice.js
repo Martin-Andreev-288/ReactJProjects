fc078a6b9511d0eba42ee76e7ab9af2ff1e11b2a
Martin Andreev <martin_i_a@abv.bg>
2024-06-24 00:54:12 +0300

    437 important update (only screenshot)

--- 14redux-toolkit-tutorial/src/features/modal/modalSlice.js
+++ 14redux-toolkit-tutorial/src/features/modal/modalSlice.js
@@ -20,8 +20,3 @@ const modalSlice = createSlice({
 export const { openModal, closeModal } = modalSlice.actions;
 
 export default modalSlice.reducer;
-
-/* pravim taka, che nadpisyt za potvyrzhdenie dali iskame
-da clear-nem cart-a da se poqvava samo ako cyknem tozi
-buton. Syotvetno pravim taka i che vyprosnite butoni da
-rabotqt kakto se ochakva */
