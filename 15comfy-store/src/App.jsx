85abc6e9783d6fc52d17d57c8d86954d78157382
Martin Andreev <martin_i_a@abv.bg>
2024-06-24 16:56:19 +0300

    444 VITE&tailwindCSS
    nachalo na proekta. Shte izpolzvame daisy, za koeto e nuzhen tailwind, taka che zapochnahme proekta s instalirane na tailwind, kakto i s vnedrqvane na izobrazheniqta
    i readme-to, koito shte ni trqbvat

---
+++ 15comfy-store/src/App.jsx
@@ -0,0 +1,6 @@
+/* nachalo na proekta. Shte izpolzvame daisy, za koeto e nuzhen tailwind, taka che zapochnahme proekta s instalirane na tailwind, kakto i s vnedrqvane na izobrazheniqta
+i readme-to, koito shte ni trqbvat */
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+export default App;
