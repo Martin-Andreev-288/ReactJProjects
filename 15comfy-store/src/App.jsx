b823baa142cfdcf47770b14e715b841ddc83ef32
Martin Andreev <martin_i_a@abv.bg>
2024-06-24 17:24:07 +0300

    447 createPages
    syzdavane na vsichki stranici (may obshto 11). Vsichki sa analogichni na About, nqma nuzhda vsqka da e na snimkata.

--- 15comfy-store/src/App.jsx
+++ 15comfy-store/src/App.jsx
@@ -1,4 +1,17 @@
-// instalirame vsichki tezi biblioteki
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
+
 const App = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
---
+++ 15comfy-store/src/pages/About.jsx
@@ -0,0 +1,5 @@
+// syzdavane na vsichki stranici (may obshto 11). Vsichki sa analogichni na About, nqma nuzhda vsqka da e na snimkata.
+const About = () => {
+  return <h1 className="text-4xl">About</h1>;
+};
+export default About;
