0708b2f14304b8a18eaee9c8588aa47d4a8b4f2f
Martin Andreev <martin_i_a@abv.bg>
2024-06-24 21:32:47 +0300

    455 header component
    syzdavame header, kydeto ima sign in / guest i create an account, koito ni prenasochvat kym
     syotvetnite linkove -> ednoto kym login, drugoto kym register

--- 15comfy-store/src/pages/HomeLayout.jsx
+++ 15comfy-store/src/pages/HomeLayout.jsx
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components";
 
 const HomeLayout = () => {
   return (
     <>
-      <nav>
-        <h1 className="text-4xl">Comfy</h1>
-      </nav>
+      <Header />
       <section className="align-element py-20">
         <Outlet />
       </section>
