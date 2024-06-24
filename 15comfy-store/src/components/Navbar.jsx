a41420f26409d2e868f2d987587ebf0369856499
Martin Andreev <martin_i_a@abv.bg>
2024-06-24 22:55:49 +0300

    457 navLinks component
    syzdavame navlinkovete (pak razlichno pri razlichni ekrani)

--- 15comfy-store/src/components/Navbar.jsx
+++ 15comfy-store/src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
-/* syzdavame navbar-a. Izpolzvame navbar ot daisyUI (pokazan e i v momenta, no v nachalen vid).
-Syshto ot tam i menu-to, dropdown-a. Tova i bez daisyUI mozhe da stane, prosto shte e poveche
-rabota. Inache pri razlichna golemina na ekrana izglezhda razlichno (sniman e i s dvete golemini) */
+import NavLinks from "./NavLinks";
 const Navbar = () => {
   return (
     <nav className="bg-base-200">
@@ -25,12 +23,14 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              nav links
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal ">nav links</ul>
+          <ul className="menu menu-horizontal ">
+            <NavLinks />
+          </ul>
         </div>
         <div className="navbar-end">
           {/* THEME ICONS */}
