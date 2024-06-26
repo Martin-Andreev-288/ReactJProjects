c48b5cc9988bc2bb2d928e5a6071c6224d9f2e6e
Martin Andreev <martin_i_a@abv.bg>
2024-06-26 17:00:34 +0300

    481 paginationSetup
    zapochvame da syzdavame stranicite (dolu vdqsno), tova, koeto lipsvashe v nashiq vtori proekt s foruma. Zasega samo logva vyrhu kakvoto sme natisnali (tova e nachaloto)

--- 15comfy-store/src/components/PaginationContainer.jsx
+++ 15comfy-store/src/components/PaginationContainer.jsx
@@ -1,4 +1,52 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
+// zapochvame da syzdavame stranicite (dolu vdqsno), tova, koeto lipsvashe v nashiq vtori proekt s foruma. Zasega samo logva vyrhu kakvoto sme natisnali (tova e nachaloto)
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const handlePageChange = (pageNumber) => {
+    console.log(pageNumber);
+  };
+
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange("prev");
+          }}
+        >
+          Prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              onClick={() => handlePageChange(pageNumber)}
+              key={pageNumber}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page ? "bg-base-300 border-base-300" : ""
+              }`}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => {
+            handlePageChange("next");
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 export default PaginationContainer;
